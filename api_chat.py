90dc1b349838adf9c9f8b191add2750904389b8e
SofiFerrarotti <106496505+SofiFerrarotti@users.noreply.github.com>
2024-08-29 10:36:12 -0300

    Update api_chat.py

--- api_chat.py
+++ api_chat.py
@@ -8,13 +8,13 @@ IMAGE_API_KEY = '3f657d8b-07d4-4d89-9da1-e57c2134c847'
 
 def generate_text_response(prompt):
     headers = {'Authorization': f'Bearer {CHAT_API_KEY}', 'Content-Type': 'application/json'}
-    data = {'inputs': prompt}  # Cambié 'prompt' a 'inputs' según la API de Hugging Face
+    data = {'inputs': prompt}  
     try:
         response = requests.post(CHAT_API_URL, headers=headers, json=data)
         response.raise_for_status()
         response_json = response.json()
-        print("Respuesta completa de la API de texto:", response_json)  # Imprime la respuesta completa
-        return response_json[0]['generated_text']  # Asegúrate de que esta es la estructura correcta de la respuesta
+        print("Respuesta completa de la API de texto:", response_json)  
+        return response_json[0]['generated_text'] 
     except requests.exceptions.RequestException as e:
         print(f"Error al hacer la solicitud: {e}")
         return ''
@@ -26,7 +26,7 @@ def generate_image(prompt):
         response = requests.post(IMAGE_API_URL, headers=headers, json=data)
         response.raise_for_status()
         response_json = response.json()
-        print("Respuesta completa de la API de imagen:", response_json)  # Imprime la respuesta completa
+        print("Respuesta completa de la API de imagen:", response_json)  
         image_url = response_json.get('image_url', '')
         return image_url
     except requests.exceptions.RequestException as e:
